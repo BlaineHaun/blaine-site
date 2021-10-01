48ba273a643bae5e89199af46c77167d9095cfae
Blaine Haun <blaine.haun@clearlink.com>
2021-10-01 11:35:42 -0600

    Added lodash to show code chunking

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,16 @@
 import React from "react";
+import _ from "lodash";
 
+const obj = {
+    one: {
+        two: {
+            three: "stuff"
+        }
+    }
+}
 const About = () => {
+    const result = _.get(obj, "one.two.three")
+    console.log(result);
     return (
         <div>
             <h1>Oh Hai, about that</h1>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,21 @@
 import * as React from "react"
 import { getRandomIntInclusive } from "../utils";
+import _ from "lodash";
 
 const rollD20 = getRandomIntInclusive(1,20)
 
+const obj = {
+    one: {
+        two: {
+            three: "stuff"
+        }
+    }
+}
+
 export default function Component () {
+    const result = _.get(obj, "one.two.three")
+    console.log(result);
+
     return (
         <div>
             <h1>Title here</h1>
